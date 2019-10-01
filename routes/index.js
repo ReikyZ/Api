50527be2be4f4a1fc64b3b1086d0dec2df4205f7
rekiyZ <reikyzhang@hotmail.com>
2019-10-01 14:06:43 +0800

    add ignore

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,23 @@ router.get('/live', function (req,res, next) {
     res.render('index', { title: 'Express live' });
 });
 
+
+/* GET p page. */
+router.get('/o', function (req, res, next) {
+    const pubFolder = 'C:/Users/Reiky/Desktop/node/Api/public/';
+    const fs = require('fs');
+    fs.readdir(pubFolder, (err, files) => {
+        files.sort(() => Math.random() - 0.5);
+        links = "";
+        files.forEach(file => {
+            if (file.endsWith(".mp4")) {
+                link = "<a href=\"/" + file + "\" target=\"_blank\"><img src='" + file + ".jpg' width='420' height='240'  title='" + file + "'></a>";
+                links += link;
+            }
+        });
+        html = "<!DOCTYPE html><html><head><title>P</title><link rel=\"stylesheet\" href=\"/stylesheets/style.css\"></head><body><p>" + links + "</p></body></html>";
+        res.send(html);
+    });
+});
+
 module.exports = router;
